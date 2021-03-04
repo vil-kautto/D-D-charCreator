7ef2ad318a680a84bbabdc45bbe4a8b850024a7f
Ville Kautto <ville.kautto@hotmail.fi>
2021-03-04 14:19:40 +0200

    Clarify function names and prepare character class for upcoming changes

---
+++ character.py
@@ -0,0 +1,10 @@
+class Character:
+    def __init__(self):
+        self.data = {"attributes": {
+            "Strength": 0,
+            "Dexterity": 0,
+            "Constitution": 0,
+            "Intelligence": 0,
+            "Wisdom": 0,
+            "Charisma": 0
+        }}
